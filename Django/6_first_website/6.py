0e09f543ce994725e2ac603f171195b4faf8eedb
talkativetushar <61559891+talkativetushar@users.noreply.github.com>
2020-07-18 08:29:01 +0530

    Add files via upload

---
+++ Django/6_first_website/6.py
@@ -0,0 +1,5 @@
+# We make a first website using django.
+# let's talk about website and web application.
+# When we make a project in django. tha project is called a website and website conation many applicaion like blog, ecoomers, forems, event, news, artical, etc. these are application.
+# let's make a first website.
+# Make a folder for first website after open teminal and go to website folder.
